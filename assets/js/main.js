7b24c04c369aac125b41eadf0e9814c0074bc8ef
SirAlyon <alessiosada04@gmail.com>
2022-04-04 19:11:12 +0200

    add comments and fix body and clear issue

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,26 +1,37 @@
 const cells = document.querySelector('.cells')
 
-//Generare i numeri in base alla difficoltà scelta dall'utente
-const difficulty = document.getElementById('difficulty').value
+document.getElementById('submit').addEventListener('click', function(){
 
-console.log(difficulty);
+    //Generare i numeri in base alla difficoltà scelta dall'utente
+    const difficulty = document.getElementById('difficulty').value
+    console.log(difficulty);
 
-//Verifico in base alla difficoltà scelta dall'utente quanti numeri generare
- if (difficulty === 'Easy'){
-    generateCells(100, 'cell','cell-width-10', 'div')
-    allCells('.cell')
-    cellsContent('.cell', 1, 100)
-} else if (difficulty === 'Medium'){
-    generateCells(81,'cell','cell-width-9', 'div')
-    allCells('.cell')
-    cellsContent('.cell', 1, 81)
-} else if (difficulty === 'Hard'){
-    generateCells(49,'cell','cell-width-7', 'div')
-    allCells('.cell')
-    cellsContent('.cell', 1, 49)
-}
+    //Azzero il contenuto di cells così mi cancella la griglia esistente prima di generarne un'altra
+    const clear = document.getElementById('clear').innerHTML = ""; 
+    
+    const body = document.getElementById('site_main')
+    body.classList.remove('body-c-height')
+    //Verifico in base alla difficoltà scelta dall'utente quanti numeri generare
+    if (difficulty === 'Easy'){
+        generateCells(100, 'cell','cell-width-10', 'div')
+        allCells('.cell')
+        cellsContent('.cell', 1, 100)
+    } else if (difficulty === 'Medium'){
+        generateCells(81,'cell','cell-width-9', 'div')
+        allCells('.cell')
+        cellsContent('.cell', 1, 81)
+    } else if (difficulty === 'Hard'){
+        generateCells(49,'cell','cell-width-7', 'div')
+        allCells('.cell')
+        cellsContent('.cell', 1, 49)
+    }
 
-cellsClick('.cell', 'active')
+    cellsClick('.cell', 'active')
+
+})
+
+
+
  
 //Inserisco i numeri generati nei in modo casuale nelle celle
 function cellsContent(select, min, max) {
@@ -47,7 +58,7 @@ function generateCells(cellsWidth, className, className2, element) { //ClassName
     }
 }
 
-
+//Funzione per generare un numero compreso tra min e max e inserire ogni valore in un array
 function generateNumbers(min, max) {
     const gridNumbers = []
     for (let i=1; i <= max; i++ ){
@@ -57,6 +68,7 @@ function generateNumbers(min, max) {
     return gridNumbers
 }
 
+//Funzione per rendere active al click l'elemento della griglia selezionato
 function cellsClick(select, className) {
     const cells = allCells(select)
     for (let i=0; i < cells.length; i++){
